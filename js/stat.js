38d701c0941e48ddcc74c3ff479223f861c7402f
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-02 10:43:36 +0500

    Добавлено белое облако с тенью. Фикс ошибки объявления функции

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,9 @@
 "use strict";
 
-window.renderStatistics(ctx, names, times) {
+window.renderStatistics = function(ctx, names, times) {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+  ctx.fillRect(110, 20, 420, 270);
 
-
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(100, 10, 420, 270);
 };
