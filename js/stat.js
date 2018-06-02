9eb1ab0f8c24d3a186181444a1f8ad8bd7bfc6ec
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-02 14:43:23 +0500

    Добавлен вывод текста в облаке

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,22 @@
 "use strict";
 
-window.renderStatistics = function(ctx, names, times) {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-  ctx.fillRect(110, 20, 420, 270);
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 
-  ctx.fillStyle = "#ffffff";
-  ctx.fillRect(100, 10, 420, 270);
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
+window.renderStatistics = function(ctx, names, times) {
+  renderCloud(ctx, 110, 20, "rgba(0, 0, 0, 0.3)");
+  renderCloud(ctx, 100, 10, "#ffffff");
+
+  ctx.fillStyle = "black";
+  ctx.font = "16px PT Mono";
+  ctx.textBaseline = "hanging";
+  ctx.textAlign = "center";
+  ctx.fillText("Ура, вы победили!", 310, 30);
+  ctx.fillText("Список результатов:", 310, 50);
+};
+
