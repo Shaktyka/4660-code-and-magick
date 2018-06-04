725e0ca728a757c3c12af0b9728df19513bcc350
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-04 09:08:33 +0500

    Правки ошибок по eslint

--- js/stat.js
+++ js/stat.js
@@ -1,35 +1,35 @@
-"use strict";
+'use strict';
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   var coeff = 20;
 
   ctx.beginPath();
   ctx.fillStyle = color;
   ctx.moveTo(x, y);
-  ctx.lineTo(x+CLOUD_WIDTH/2, y+coeff);
-  ctx.lineTo(x+CLOUD_WIDTH, y);
-  ctx.lineTo(x+CLOUD_WIDTH - coeff, y+CLOUD_HEIGHT/2);
-  ctx.lineTo(x+CLOUD_WIDTH, y+CLOUD_HEIGHT);
-  ctx.lineTo(x+CLOUD_WIDTH-CLOUD_WIDTH/2, y+CLOUD_HEIGHT-coeff);
-  ctx.lineTo(x, y+CLOUD_HEIGHT);
-  ctx.lineTo(x+coeff, y+CLOUD_HEIGHT/2);
+  ctx.lineTo(x + CLOUD_WIDTH / 2, y + coeff);
+  ctx.lineTo(x + CLOUD_WIDTH, y);
+  ctx.lineTo(x + CLOUD_WIDTH - coeff, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x + CLOUD_WIDTH, y + CLOUD_HEIGHT);
+  ctx.lineTo(x + CLOUD_WIDTH - CLOUD_WIDTH / 2, y + CLOUD_HEIGHT - coeff);
+  ctx.lineTo(x, y + CLOUD_HEIGHT);
+  ctx.lineTo(x + coeff, y + CLOUD_HEIGHT / 2);
   ctx.closePath();
   ctx.stroke();
   ctx.fill();
 };
 
-window.renderStatistics = function(ctx, names, times) {
-  renderCloud(ctx, 110, 20, "rgba(0, 0, 0, 0.7)");
-  renderCloud(ctx, 100, 10, "#ffffff");
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#ffffff');
 
-  ctx.fillStyle = "#000000";
-  ctx.font = "16px PT Mono";
-  ctx.textBaseline = "hanging";
-  ctx.textAlign = "center";
-  ctx.fillText("Ура, вы победили!", 310, 40);
-  ctx.fillText("Список результатов:", 310, 60);
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.textAlign = 'center';
+  ctx.fillText('Ура, вы победили!', 310, 40);
+  ctx.fillText('Список результатов:', 310, 60);
 };
 
