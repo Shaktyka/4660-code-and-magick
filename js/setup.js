4a7d8fefa578c271458c8776c1f65051aa8ab510
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-06 10:58:27 +0500

    Заведены массивы составных элементов магов

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,11 @@
 var setup = document.querySelector('div.setup');
 
 setup.classList.remove('hidden');
+
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+
+var fornames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
