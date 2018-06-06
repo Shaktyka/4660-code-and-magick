10e712036837fed750b535bcb72e7305d31dd7f9
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-06 11:27:24 +0500

    Добавлена функция случайного выбора элемента массива

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var setup = document.querySelector('div.setup');
 
 setup.classList.remove('hidden');
@@ -9,3 +11,15 @@ var fornames = ['да Марья', 'Верон', 'Мирабелла', 'Валь
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandElem = function (massiveName) {
+  var randElem = Math.floor(Math.random() * massiveName.length);
+  return massiveName[randElem];
+};
+
+// Объект Волшебник
+// {
+//    name: 'составное имя',
+//    coatColor: '',
+//    eyesColor: ''
+// }
