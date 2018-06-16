153ff3bb81e9ecfddf1733210d5a7715c9d9a16e
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-16 11:03:43 +0500

    Добавлено перемещение окна настройки персонажа

--- js/dialog.js
+++ js/dialog.js
@@ -1,1 +1,45 @@
 'use strict';
+
+var setup = document.querySelector('.setup');
+
+// Находим то, что будем перетаскивать
+var dialogHandle = setup.querySelector('.setup-user-pic');
+
+// Добавляем обработчик 1 фазы события - mousdown
+dialogHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  // Записываем стартовые координаты
+  var startCoordinates = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  // При каждом движении мыши обновляем смещение относительно первоначальной точки
+  var mouseMoveHandler = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = {
+      x: startCoordinates.x - moveEvt.clientX,
+      y: startCoordinates.y - moveEvt.clientY
+    };
+    var startCoordinates = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  };
+  // При отжатии кнопки мыши перестаём слушать перемешение курсора
+  var mouseUpHandler = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+  };
+  // Добавляем обработчики событий на движение мыши и на отжатие кнопки мыши
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+});
+
+//  Реализуйте возможность перетаскивания диалога:
+//
+//  1) Диалог должен начинать двигаться за курсором мыши при нажатии (mousedown) на блок .setup-user-pic;
+//  2) Диалог должен переставать двигаться за курсором мыши при отпускании (mouseup) кнопки мыши и оставаться на новом месте;
+//  3) При повторном открытии/закрытии диалога положение диалога должно сбрасываться на изначальное;
