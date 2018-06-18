de285e8ec7b9a74c1597c79d92191b48bed3d4a2
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-18 12:42:51 +0500

    Попытки починить перетаскивание

--- js/dialog.js
+++ js/dialog.js
@@ -29,15 +29,15 @@ dialogHandler.addEventListener('mousedown', function (evt) {
     setup.style.top = (setup.offsetTop - shift.y) + 'px';
     setup.style.left = (setup.offsetLeft - shift.x) + 'px';
   };
-  // При отжатии кнопки мыши перестаём слушать перемешение курсора
+  // При отжатии кнопки мыши перестаём слушать перемещение курсора
   var mouseUpHandler = function (upEvt) {
     upEvt.preventDefault();
     document.removeEventListener('mousemove', mouseMoveHandler);
     document.removeEventListener('mouseup', mouseUpHandler);
   };
   if (dragged) {
-    var onClickPreventDefault = function (prevEvt) {
-      prevEvt.preventDefault();
+    var onClickPreventDefault = function (preEvt) {
+      preEvt.preventDefault();
       dialogHandler.removeEventListener('click', onClickPreventDefault);
     };
     dialogHandler.addEventListener('click', onClickPreventDefault);
@@ -46,9 +46,3 @@ dialogHandler.addEventListener('mousedown', function (evt) {
   document.addEventListener('mousemove', mouseMoveHandler);
   document.addEventListener('mouseup', mouseUpHandler);
 });
-
-//  Реализуйте возможность перетаскивания диалога:
-//
-//  1) Диалог должен начинать двигаться за курсором мыши при нажатии (mousedown) на блок .setup-user-pic;
-//  2) Диалог должен переставать двигаться за курсором мыши при отпускании (mouseup) кнопки мыши и оставаться на новом месте;
-//  3) При повторном открытии/закрытии диалога положение диалога должно сбрасываться на изначальное;
