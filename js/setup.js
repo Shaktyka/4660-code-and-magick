6b6ed10e756c959fafcc6d290eb95cd8d5bb9efb
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-25 12:09:18 +0500

    Открытие-закрытие окна выделено в отдельный модуль

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
-
 var userDialog = document.querySelector('.setup');
 
 // ГЕНЕРАЦИЯ ОДНОГО МАГА
@@ -85,61 +82,6 @@ similarListElement.appendChild(fragment);
 
 // Убираем класс hidden у блока для похожих магов
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
-
-// ОТКРЫТИЕ-ЗАКРЫТИЕ ОКНА НАСТРОЕК
-
-// Окно-попап с настройками персонажа
-
-// var userDialog = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-
-// Кнопка для закрытия окна настроек персонажа
-
-var setupClose = userDialog.querySelector('.setup-close');
-
-// Стартовые координаты окна
-var startCoords = {
-  x: userDialog.style.top,
-  y: userDialog.style.left
-};
-
-var onPopupEscPress = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    closePopup();
-  }
-};
-
-var openPopup = function () {
-  userDialog.style.top = startCoords.y;
-  userDialog.style.left = startCoords.x;
-  userDialog.classList.remove('hidden');
-  document.addEventListener('keydown', onPopupEscPress);
-};
-
-var closePopup = function () {
-  userDialog.classList.add('hidden');
-  document.removeEventListener('keydown', onPopupEscPress);
-};
-
-setupOpen.addEventListener('click', function () {
-  openPopup();
-});
-
-setupOpen.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    openPopup();
-  }
-});
-
-setupClose.addEventListener('click', function () {
-  closePopup();
-});
-
-setupClose.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    closePopup();
-  }
-});
 
 // НАСТРОЙКА ВНЕШНЕГО ВИДА МАГА
 
