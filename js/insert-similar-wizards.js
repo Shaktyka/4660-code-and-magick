3f0e7fc67b7c2ef46bda2522606b8d94ee5b1241
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-25 18:14:24 +0500

    Правки

--- js/insert-similar-wizards.js
+++ js/insert-similar-wizards.js
@@ -3,13 +3,16 @@
 (function () {
   var userDialog = document.querySelector('.setup');
 
+  // Находим блок, куда будем вставлять сгенерированный список.
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+
   // Берём массив магов, формируем карточку и добавляем её в целевой блок.
   var fragment = document.createDocumentFragment();
   var wizards = window.getWizardsArray(4);
   for (var i = 0; i < wizards.length; i++) {
     fragment.appendChild(window.renderWizard(wizards[i]));
   }
-  window.similarListElement.appendChild(fragment);
+  similarListElement.appendChild(fragment);
 
   // Убираем класс hidden у блока для похожих магов
   userDialog.querySelector('.setup-similar').classList.remove('hidden');
