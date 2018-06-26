a495f6ef85b80bc368676feb67e1640585e06a5d
Elena <k-i-s-u-n-j-a@list.ru>
2018-06-26 09:40:58 +0500

    Убраны комментарии

--- js/utils.js
+++ js/utils.js
@@ -20,9 +20,4 @@ window.util = (function () {
       return array[randomElement];
     }
   };
-  // Функция, которая выбирает рандомный элемент из любого переданного ей массива.
-//  window.getRandomElement = function (array) {
-//    var randomElement = Math.floor(Math.random() * array.length);
-//    return array[randomElement];
-//  };
 })();
