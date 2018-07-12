c856d27bc369fc64f614d407044d864e80fae8b4
Elena <k-i-s-u-n-j-a@list.ru>
2018-07-12 11:36:09 +0500

    Переименование StartCoords

--- js/popup.js
+++ js/popup.js
@@ -11,7 +11,7 @@
   var setupClose = window.userDialog.querySelector('.setup-close');
 
   // Стартовые координаты окна
-  var startCoords = {
+  var StartCoords = {
     x: window.userDialog.style.top,
     y: window.userDialog.style.left
   };
@@ -22,8 +22,8 @@
   };
 
   var openPopup = function () {
-    window.userDialog.style.top = startCoords.y;
-    window.userDialog.style.left = startCoords.x;
+    window.userDialog.style.top = StartCoords.y;
+    window.userDialog.style.left = StartCoords.x;
     window.userDialog.classList.remove('hidden');
     document.addEventListener('keydown', escKeydownHandler);
   };
