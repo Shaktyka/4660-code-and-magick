c7f337aa2117126549d7a0801229d95b79f5fcfe
Elena <k-i-s-u-n-j-a@list.ru>
2018-07-12 14:02:13 +0500

    Улучшение структуры файла

--- js/customization.js
+++ js/customization.js
@@ -4,13 +4,32 @@
 
 (function () {
 
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
 
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
 
-  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var FIREBALL_COLORS = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+  ];
 
-  // Находим в разметке нужные элементы: настраиваемые элементы мага и скрытые поля
+  // Настраиваемые элементы мага и скрытые поля
 
   var setupPlayer = window.userDialog.querySelector('.setup-player');
 
